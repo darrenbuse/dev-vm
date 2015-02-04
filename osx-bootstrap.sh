19e37683b47b98414cabac16fd3fb13966e7685a
Darren Buse <darren.buse@gmail.com>
2015-02-04 18:32:22 +0000

    Rename vm

--- osx-bootstrap.sh
+++ osx-bootstrap.sh
@@ -2,5 +2,4 @@
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install ansible
 brew install caskroom/cask/brew-cask
-#ansible-playbook osx.yml -i inventory
 
